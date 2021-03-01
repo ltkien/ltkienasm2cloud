3590955d18760713120b38d72fb40b22f2a5119b
ltkien <66240745+ltkien@users.noreply.github.com>
2021-03-01 10:41:53 +0700

    Update Toy.php

--- Toy.php
+++ Toy.php
@@ -10,9 +10,9 @@ $query = "select id, toy_name, toy_price, toy_category, atn_store, toy_quantity,
 
 
 $result = pg_query($con, $query);
-echo ("hi");
-$resultCheck = pg_num_rows($result);
 
+$resultCheck = pg_num_rows($result);
+echo ("hi");
 ?>
 
 
