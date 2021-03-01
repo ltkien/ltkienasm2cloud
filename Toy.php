6ac83b3981803d6999d016ab0ef114a4c37c67d5
ltkien <66240745+ltkien@users.noreply.github.com>
2021-03-01 10:28:52 +0700

    Update Toy.php

--- Toy.php
+++ Toy.php
@@ -1,5 +1,5 @@
 <?php
-echo('hi');
+
 
 $con = pg_connect("host=ec2-54-157-149-88.compute-1.amazonaws.com 
 dbname=d7li5vh6npno94
@@ -11,6 +11,7 @@ sslmode=require");
 $query = "select id, toy_name, toy_price, toy_category, atn_store, toy_quantity, toy_description from toy ;";
 $result = pg_query($con, $query);
 $resultCheck = pg_num_rows($result);
+echo('hi');
 ?>
 
 
