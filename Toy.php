551b6d1f02def8d5df7a66462e6be85e15f107e5
ltkien <66240745+ltkien@users.noreply.github.com>
2021-03-01 11:55:25 +0700

    Update Toy.php

--- Toy.php
+++ Toy.php
@@ -326,7 +326,7 @@ echo ("hi");
                   <?php echo $row['toy_description']; ?>
                 </td>
                 <td>
-                  <div class="delete" data-toggle="buttons"><a href="productdelete.php?product_id=<?php echo $row["product_id"]; ?>">DELETE</a></div>
+                  <div class="delete" data-toggle="buttons"><a href="toydelete.php?id=<?php echo $row["id"]; ?>">DELETE</a></div>
                 </td>
               </tr>
           <?php
